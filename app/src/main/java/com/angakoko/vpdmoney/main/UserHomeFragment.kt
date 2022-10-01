c2fad4be94d6ad27b080f55b44cad4cf4af29b93
anga <angakoko@gmail.com>
2022-10-01 19:17:16 +0100

    Design fragment_user_home.xml

--- app/src/main/java/com/angakoko/vpdmoney/main/UserHomeFragment.kt
+++ app/src/main/java/com/angakoko/vpdmoney/main/UserHomeFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.angakoko.vpdmoney.R
+import com.angakoko.vpdmoney.ViewModelFactory
+import com.angakoko.vpdmoney.databinding.FragmentUserHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +18,34 @@ import com.angakoko.vpdmoney.R
  */
 class UserHomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentUserHomeBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_home, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_user_home, container, false)
+
+        val viewModelFactory = ViewModelFactory(requireContext(), requireActivity().application)
+        viewModel = ViewModelProvider(requireActivity(), viewModelFactory).get(MainViewModel::class.java)
+
+        //Set the header to [Users]
+        viewModel.setHeader(getString(R.string.users))
+
+        //Listen to on new button click
+        binding.fab.setOnClickListener {
+            //Navigate to new user fragment when new button is clicked
+            findNavController().navigate(R.id.action_userHomeFragment_to_newUserFragment)
+        }
+
+        binding.swipeRefresh.setOnRefreshListener {
+            binding.swipeRefresh.isRefreshing = false
+        }
+
+        binding.lifecycleOwner = requireActivity()
+        binding.viewModel = viewModel
+        return binding.root
     }
 }
