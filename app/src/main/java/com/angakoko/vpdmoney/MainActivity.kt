370408c27adf500efbeb56e1180abb11741828d1
anga <angakoko@gmail.com>
2022-10-01 17:07:19 +0100

    Design main activity

--- app/src/main/java/com/angakoko/vpdmoney/MainActivity.kt
+++ app/src/main/java/com/angakoko/vpdmoney/MainActivity.kt
@@ -2,10 +2,27 @@ package com.angakoko.vpdmoney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.angakoko.vpdmoney.databinding.ActivityMainBinding
+import com.angakoko.vpdmoney.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val viewModelFactory = ViewModelFactory(this, application)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        binding.back.setOnClickListener {
+            onBackPressed()
+        }
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
--- app/src/main/java/com/angakoko/vpdmoney/main/MainViewModel.kt
+++ app/src/main/java/com/angakoko/vpdmoney/main/MainViewModel.kt
@@ -3,6 +3,11 @@ package com.angakoko.vpdmoney.main
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 
 class MainViewModel(private val context: Context, application: Application): AndroidViewModel(application) {
+
+    private val header: MutableLiveData<String> = MutableLiveData()
+    fun getHeader(): MutableLiveData<String> = header
+    fun setHeader(string: String){header.value = string}
 }
